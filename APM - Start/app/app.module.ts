06c923a088eb87332a9cb90a2b9e27c498e00e45
Bjørg Fredriksen <bjorg@promineo.no>
2016-12-21 14:55:03 +0100

    updated

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ProductListComponent } from './products/product-list.component';
 import { AppComponent }  from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports: [ BrowserModule,
+  FormsModule ],
+  declarations: [ AppComponent, 
+  ProductListComponent],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
